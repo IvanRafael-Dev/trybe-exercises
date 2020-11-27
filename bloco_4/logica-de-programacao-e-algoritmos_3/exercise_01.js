46e67a220246666f80ceb2738da7cf998533e0c4
IvanRafael <ivanrafael.dev@gmail.com>
2020-11-27 18:36:00 -0300

    Exercicio 01

---
+++ bloco_4/logica-de-programacao-e-algoritmos_3/exercise_01.js
@@ -0,0 +1,16 @@
+/* 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n; 
+n = 5;
+*****
+*****
+*****
+*****
+*****
+*/
+
+let n = 5;
+let sinal = '*';
+for (let index = 0; index < n; index += 1){
+    console.log(sinal.repeat(n))
+}
+
+
