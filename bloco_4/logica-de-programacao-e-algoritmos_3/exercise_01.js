7d2d5f21c5e4fb25dc007bcd9b3fa217336a6445
IvanRafael <ivanrafael.dev@gmail.com>
2020-11-27 23:12:53 -0300

    Exercicio 03

--- bloco_4/logica-de-programacao-e-algoritmos_3/exercise_01.js
+++ bloco_4/logica-de-programacao-e-algoritmos_3/exercise_01.js
@@ -8,9 +8,7 @@ n = 5;
 */
 
 let n = 5;
-let sinal = '*';
-for (let index = 0; index < n; index += 1){
-    console.log(sinal.repeat(n))
+let sinal = "*";
+for (let index = 0; index < n; index += 1) {
+  console.log(sinal.repeat(n));
 }
-
-
