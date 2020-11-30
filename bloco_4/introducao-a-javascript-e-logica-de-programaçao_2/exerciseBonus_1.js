efebcf270211b217aaa4289731cfd24f8816ae4f
IvanRafael <ivanrafael.dev@gmail.com>
2020-11-30 12:30:49 -0300

    Troca sinal incremento (i++) para i += 1

--- bloco_4/introducao-a-javascript-e-logica-de-programaçao_2/exerciseBonus_1.js
+++ bloco_4/introducao-a-javascript-e-logica-de-programaçao_2/exerciseBonus_1.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 1; i < numbers.length; i++) {
-  for (let j = 0; j < i; j++) {
+for (let i = 1; i < numbers.length; i += 1) {
+  for (let j = 0; j < i; j += 1) {
     if (numbers[i] < numbers[j]) {
       let position = numbers[i];
       numbers[i] = numbers[j];
