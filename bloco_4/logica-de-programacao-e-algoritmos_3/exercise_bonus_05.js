84c8668d79d333f31e8fd14b9f7e6a72705c8856
IvanRafael <ivanrafael.dev@gmail.com>
2020-12-04 15:23:46 -0300

    exercicio 5 & 6 bloco 4.3

---
+++ bloco_4/logica-de-programacao-e-algoritmos_3/exercise_bonus_05.js
@@ -0,0 +1,31 @@
+/* Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+n = 7
+
+   *
+  * *
+ *   *
+*******
+
+*/
+let n = 7;
+let middle = (n+1)/2;
+let asterisco = '*';
+let right = middle;
+let left = middle;
+let output = '';
+
+for (let line = 1; line <= middle; line++) {
+   output = '';
+   for (let column = 1; column <= n; column++) {
+      if (column == right || column == left || line == middle) {
+         output += asterisco;
+      } else {
+         output += ' ';
+      }
+   }
+   right += 1;
+   left -= 1;
+   console.log(output);
+}
+
