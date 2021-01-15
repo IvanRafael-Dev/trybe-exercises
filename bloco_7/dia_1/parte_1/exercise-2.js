b89e352b0e5707b8fcbacbcf11417dc7f89ba5da
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-15 11:00:29 -0300

    Exercicio 2 com bubbleSort

---
+++ bloco_7/dia_1/parte_1/exercise-2.js
@@ -0,0 +1,16 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+function orderNumbers(array) {
+	for (let i = 1; i < array.length; i += 1) {
+		for (let j = 0; j < array.length; j += 1) {
+			if(array[j] > array[i]) {
+				let aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+			}
+		}
+	}
+	return array;
+}
+
+console.log(orderNumbers(oddsAndEvens));
