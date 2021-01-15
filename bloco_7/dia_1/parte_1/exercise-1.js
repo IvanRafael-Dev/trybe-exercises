952054744403cf3e4b9102b45be04064f16cc70f
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-15 10:50:46 -0300

    Exercicio 1 corrigindo identação

--- bloco_7/dia_1/parte_1/exercise-1.js
+++ bloco_7/dia_1/parte_1/exercise-1.js
@@ -1,20 +1,20 @@
-const testingScope = (escopo) => { 
-    if (escopo) { 
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
-    } else {
-      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
+const testingScope = (escopo) => {
+  if (escopo) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
   }
+};
 testingScope(true);
 
 ///////////////////////////////////////////////////////////////////////////////
 
 const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-const goodUse = 'ótimo, fui utilizada no escopo !'
+const goodUse = 'ótimo, fui utilizada no escopo !';
 const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-
-const testingScope2 = escopo =>  (escopo) ? `${ifScope} ${goodUse}` : elseScope;
+const testingScope2 = (escopo) =>
+  escopo ? `${ifScope} ${goodUse}` : elseScope;
 console.log(testingScope2(true));
