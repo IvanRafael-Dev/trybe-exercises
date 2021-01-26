1c51b821063f68ee6f4dad16b4a60c363e184aba
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-26 17:10:34 -0300

    Exercicio parte 3 do dia_1

---
+++ bloco_5/dia_1/exAulaParte3/script.js
@@ -0,0 +1,8 @@
+document.querySelector('header').style.backgroundColor = '#59B169';
+document.querySelector('.emergency-tasks').style.backgroundColor = '#F59D83';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = '#F9DB5E';
+document.querySelectorAll('.emergency-tasks h3')
+.forEach(element => element.style.backgroundColor = '#AE73F4');
+document.querySelectorAll('.no-emergency-tasks h3')
+.forEach(element => element.style.backgroundColor = '#000');
+document.querySelector('#footer-container').style.backgroundColor = '#123633';
