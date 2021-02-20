236863bd47a91d07e015be6b4a207d925641f7ec
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-02-20 17:25:54 -0300

    atualizando repositorio de exercicios

--- bloco_11/dia_1/exercise/src/App.test.js
+++ bloco_11/dia_1/exercise/src/App.test.js
@@ -3,14 +3,14 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  
+
   const taskJavascript = screen.getByText(/Javascript/i);
   expect(taskJavascript).toBeInTheDocument();
 
-  const React = screen.getByText(/React/i);
-  expect(React).toBeInTheDocument();
+  const taskReact = screen.getByText(/React/i);
+  expect(taskReact).toBeInTheDocument();
 
-  const Jest = screen.getByText(/Jest/i);
-  expect(Jest).toBeInTheDocument();
+  const taskJest = screen.getByText(/Jest/i);
+  expect(taskJest).toBeInTheDocument();
 
 });
