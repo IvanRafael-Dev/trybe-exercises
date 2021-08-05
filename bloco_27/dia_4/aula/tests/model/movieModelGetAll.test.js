8f8d6408d49822417fb96b26caa3d4ebd06a240a
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-08-05 18:39:20 -0300

    movieModel getAll tests

--- bloco_27/dia_4/aula/tests/model/movieModelGetAll.test.js
+++ bloco_27/dia_4/aula/tests/model/movieModelGetAll.test.js
@@ -1,15 +1,44 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
+const MovieModel = require('../../models/movieModel');
 
-describe('Busca todos os filmes do DB', () => {
+describe('Busca todos os filmes do DB', () => { // qual parte do codigo vou testar?
   // arrange // act // assert  
-  describe('quando não existe nenhum filme cadastrado', () => {
-    it('ele retorna um array', async () => {
+  describe('quando não existe nenhum filme cadastrado', () => { // caso de uso
 
+    it('ele retorna um array', async () => { // o que eu quero
+      const result = await MovieModel.getAll();
+      expect(result).to.be.an('array')
     });
-    
-    it('o array é vazio', async () => {
 
+    it('o array é vazio', async () => {
+      const result = await MovieModel.getAll();
+      expect(result).to.be.empty; 
     });
   });
+
+  describe('Quando existe pelo menos um filme cadastrado', () => {
+    
+    it('retorna um array', async () => {
+      const result = await MovieModel.getAll();
+      expect(result).to.be.an('array');
+    });
+
+    it('o array não é vazio', async () => {
+      const result = await MovieModel.getAll();
+      expect(result).to.not.be.empty;
+    });
+
+    it('o array possui itens do tipo Object', async () => {
+      const [ item ] = await MovieModel.getAll();
+      expect(item).to.be.an('object');
+    });
+
+    it('os objetos do array possuem as keys, "id", "title", "releaseYear", "directedBy" ', async () => {
+      const [ item ] = await MovieModel.getAll();
+      expect(item).to.include.all.keys('id', 'title', 'releaseYear', 'directedBy');
+    });
+    
+  });
 });
+
