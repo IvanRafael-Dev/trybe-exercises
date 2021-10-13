923e9394d0f9e1f9286302b65570724bfa44c80e
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-10-13 17:33:55 -0300

    chance class User to use sequelize.define function

--- bloco_29/dia_2/course/models/user.js
+++ bloco_29/dia_2/course/models/user.js
@@ -1,23 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    fullName: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+const User = (sequelize, DataTypes) => 
+  sequelize.define("User", {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+})
+
+module.exports = User
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   User.init({
+//     fullName: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
