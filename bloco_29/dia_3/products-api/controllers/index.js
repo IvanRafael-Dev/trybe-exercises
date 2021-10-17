3cdb9a51414e6f426ec379434634633986bbe9a2
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-10-17 13:02:56 -0300

    refazendo

--- bloco_29/dia_3/products-api/controllers/index.js
+++
@@ -1,7 +0,0 @@
-const ping = require('./ping');
-const products = require('./products');
-
-module.exports = {
-  ping,
-  products,
-};
--- bloco_29/dia_3/products-api/models/product.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Product = sequelize.define('Product', {
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    price: DataTypes.FLOAT,
-  });
-
-  return Product;
-};
